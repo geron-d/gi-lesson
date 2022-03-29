71ca5213116cf036ce79e8276867870d01b17cf7
geron_d <geron_d@mail.ru>
2022-03-29 20:05:14 +0300

    rebase

--- src/main/java/by/it/academy/App.java
+++ src/main/java/by/it/academy/App.java
@@ -7,7 +7,14 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        printValue();
+
+    }
+
+    private static void printValue() {
         System.out.println(a + b);
+        System.out.println(a * b + 1);
         System.out.println(a / b);
+        
     }
 }
